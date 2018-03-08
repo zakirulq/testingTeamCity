627ef7df6bca1b89fa61c58b6d6365a23c509a97
AKM Zakirul Quayum <zakloon4@hotmail.com>
2018-03-08 14:22:25 +1100

    First Commit

---
+++ UnitTest.TeamCity/TestCalculator.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using TestTeamCity;
+
+namespace UnitTest.TeamCity
+{
+  [TestFixture]
+  public class TestCalculator
+  {
+    [TestCase(1,2,3)]
+    [TestCase(2,2,4)]
+    public void TestSum(int a, int b, int expectedResult)
+    {
+      var calculator = new Calculator();
+      var result = calculator.Sum(a, b);
+      Assert.AreEqual(expectedResult, result);
+    }
+  }
+}
